1838e2d4ca00466bea15e42f2d4de57112d1d961
Sviatoslav <ecrprague.asi@gmail.com>
2022-09-23 19:28:57 +0300

    Task2 Done

---
+++ HW_Task2/Program.cs
@@ -0,0 +1,20 @@
+﻿Console.WriteLine("Введите число: ");
+int num=Convert.ToInt32(Console.ReadLine());
+
+if (num < 100) Console.WriteLine("Третьей цифры нет");
+
+if (num >=100 && num<1000)
+{
+    num=num%10;
+    Console.WriteLine($"Число {num} является третьим");
+}
+if (num >=1000 && num<10000)
+{
+    num=num/10%10;
+    Console.WriteLine($"Число {num} является третьим");
+}
+if (num >=10000 && num<100000)
+{
+    num=num/100%10;
+    Console.WriteLine($"Число {num} является третьим");
+}
