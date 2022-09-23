65a47a0cb7856bd8f0c5a6ddc6e4fcad884eb5a5
Sviatoslav <ecrprague.asi@gmail.com>
2022-09-23 18:41:59 +0300

    Task1 Done

--- HW_Task1/Program.cs
+++ HW_Task1/Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Введите трехначное число: ");
+int num=Convert.ToInt32(Console.ReadLine());
+
+bool isIt000(int number)
+{
+    return number>99 && number < 10000;
+}
+
+if (isIt000(num))
+{
+    num=num%100/10;
+    Console.WriteLine($"Второе число: {num}.");
+}
+else Console.WriteLine("Трехначное число, камрад.");
