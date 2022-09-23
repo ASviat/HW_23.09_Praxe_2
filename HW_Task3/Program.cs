e20d35a17c34c3b3d0e14e10bb44f9e4bbd53b56
Sviatoslav <ecrprague.asi@gmail.com>
2022-09-23 19:43:25 +0300

    Task3 Done

---
+++ HW_Task3/Program.cs
@@ -0,0 +1,6 @@
+﻿Console.WriteLine("Введите число: ");
+int num=Convert.ToInt32(Console.ReadLine());
+
+if (num >0 && num<=5) Console.WriteLine("Нет, будний.");
+if (num==6||num==7) Console.WriteLine("Да, выходной.");
+if (num<1||num>7) Console.WriteLine ("В неделе 7 дней.");
